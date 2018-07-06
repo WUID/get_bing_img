f9df5eaab80b61d7cb984b264cd3bd44c2fa73af
wuid <3310975439@qq.com>
2018-07-06 11:47:05 +0800

    完成

--- main.go
+++ main.go
@@ -1,21 +1,60 @@
 package main
 
 import (
+	"io"
+	"os"
     "fmt"
+    "time"
+    "strings"
     "net/http"
     "io/ioutil"
 )
 
-func main(){
-    fmt.Println("hello")
+func get_img_url() string {
+
     resp, err := http.Get("https://cn.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1")
     if err != nil {
+        fmt.Println(err)
     }
-    defer resp.Body.Close()
-    body, err := ioutil.ReadAll(resp.Body)
 
+    data, err := ioutil.ReadAll(resp.Body)
     if err != nil {
+        fmt.Println(err)
     }
-    fmt.Println(string(body))
+
+    defer resp.Body.Close()
+
+    ret_json := string(data)
+    url_start := strings.Index(ret_json, `/az/hprichbg/rb/`)
+    url_end := strings.Index(ret_json, `","urlbase"`)
+    img_url := string("")
+
+    for i := url_start;i < url_end;i++ {
+        img_url += string(ret_json[i])
+    }
+
+    return ("http://www.bing.com" + img_url)
+}
+
+func dowload_img(url string) {
+
+    res, err := http.Get(url)
+	if err != nil {
+		fmt.Println(err)
+    }
+    
+    f, err := os.Create(time.Now().Format("2006-01-02 15:04:05") + ".jpg")
+	if err != nil {
+		fmt.Println(err)
+    }
+    
+    io.Copy(f, res.Body)
+    
+}
+
+func main(){
+
+    dowload_img(get_img_url())
+
 }
 
